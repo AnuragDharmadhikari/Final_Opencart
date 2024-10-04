657d0e6e52e3f70c1f36a0bda0d6eac123d74fe3
AnuragDharmadhikari <dharmadhikarianurag61@gmail.com>
2024-10-04 11:04:39 +0530

    Final Commit

--- src/test/java/pageObjects/HomeInsurancePage.java
+++ src/test/java/pageObjects/HomeInsurancePage.java
@@ -1,91 +1,101 @@
 package pageObjects;
-
+ 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+ 
+import utilities.ExcelUtility;
+ 
 public class HomeInsurancePage extends BasePage {
-
+ 
 	JavascriptExecutor js = (JavascriptExecutor) driver;
-
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+ 
 	public HomeInsurancePage(WebDriver driver) {
 		super(driver);//Invoking the BasePage Constructor
 	}
-
+ 
 	//Located all the webElements necessary.
 	@FindBy(xpath = "//input[@id='homename_hi']")
 	WebElement name;
-
+ 
 	@FindBy(xpath = "//input[@id='homemobile_hi']")
 	WebElement mobile;
-
+ 
 	@FindBy(xpath = "//div[text()='View Free Quotes']")
 	WebElement freeQuotesBtn;
-
-	@FindBy(xpath = "//span[normalize-space()='Pune']")
-	WebElement enterCity;
-
-	@FindBy(xpath = "//div[@class='proceedBtn btnHIStep2 loader']")
+ 
+	@FindBy(xpath="//label[@for='home-insurance']")
+	WebElement selectHomeInsPln;
+ 
+	@FindBy(xpath="//div[@class='proceedBtn btnHIStep2 loader']")
 	WebElement clickViewPlans;
-
+ 
 	@FindBy(xpath = "//input[@id='StructureValue']")
 	WebElement houseValue;
-
+ 
 	@FindBy(xpath = "//input[@id='ContentValue']")
 	WebElement houseItemsValue;
-
+ 
 	@FindBy(xpath = "//button[normalize-space()='View Prices']")
 	WebElement viewPricesBtn;
-
+ 
 	@FindBy(xpath = "//div[@class=' insurers-plan']")
 	List<WebElement> insurersPlans;
-
+ 
 	
 	//Added all the action methods necessary.
 	public void enterName() {
-		name.sendKeys("Ram");
+		name.sendKeys("Kunal");
 	}
-
+ 
 	public void enterMobileNo() {
 		mobile.sendKeys("9087612543");
 	}
-
+ 
 	public void clickQuotesButton() {
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		wait.until(ExpectedConditions.elementToBeClickable(freeQuotesBtn));
 		js.executeScript("arguments[0].click();", freeQuotesBtn);
 	}
-
-	public void selectCityName() {
-		js.executeScript("arguments[0].click();", enterCity);
-
+ 
+	public void selectHomeIns() {
+		selectHomeInsPln.click();
 	}
-
-	public void clickViewPlanBtn() {
+ 
+	public void clickViewPlanBtn() throws InterruptedException {
+//		Thread.sleep(5000);
+		wait.until(ExpectedConditions.elementToBeClickable(clickViewPlans));
 		js.executeScript("arguments[0].click();", clickViewPlans);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[normalize-space()='View Prices']")));
 	}
-
+ 
 	public void enterHouseVlaue() {
 		houseValue.sendKeys("5000000");
 	}
-
+ 
 	public void enterHouseItemsValue() {
 		houseItemsValue.sendKeys("500000");
 	}
-
+ 
 	public void clickPricesBtn() {
 		viewPricesBtn.click();
 	}
-
-	public void insurersPlansList() {
-		for (WebElement ls : insurersPlans) {
-			System.out.println(ls.getText());
-		}
+ 
+	public  void insurersPlansListForExcel() throws IOException {
+		String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		
+		String path = "C:\\Users\\2355163\\eclipse-workspace\\FinalProject\\testData\\Home"+timeStamp+".xlsx";
+		ExcelUtility eu=new ExcelUtility(path);
+		eu.createExcelFile(insurersPlans);
 	}
 }
--- src/test/java/utilities/ExcelUtility.java
+++ src/test/java/utilities/ExcelUtility.java
@@ -1,21 +1,23 @@
 package utilities;
-
+ 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
+import java.util.List;
+ 
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.ss.usermodel.FillPatternType;
-import org.apache.poi.ss.usermodel.IndexedColors;
+//import org.apache.poi.ss.usermodel.FillPatternType;
+//import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
+import org.openqa.selenium.WebElement;
+ 
 public class ExcelUtility {
-
+ 
 	public FileInputStream fi;
 	public FileOutputStream fo;
 	public XSSFWorkbook workbook;
@@ -24,11 +26,11 @@ public class ExcelUtility {
 	public XSSFCell cell;
 	public CellStyle style;
 	String path;
-
+ 
 	public ExcelUtility(String path) {
 		this.path = path;
 	}
-
+ 
 	public int getRowCount(String sheetName) throws IOException {
 		fi = new FileInputStream(path);
 		workbook = new XSSFWorkbook(fi);
@@ -38,7 +40,7 @@ public class ExcelUtility {
 		fi.close();
 		return rowcount;
 	}
-
+ 
 	public int getCellCount(String sheetName, int rownum) throws IOException {
 		fi = new FileInputStream(path);
 		workbook = new XSSFWorkbook(fi);
@@ -49,14 +51,14 @@ public class ExcelUtility {
 		fi.close();
 		return cellcount;
 	}
-
+ 
 	public String getCellData(String sheetName, int rownum, int colnum) throws IOException {
 		fi = new FileInputStream(path);
 		workbook = new XSSFWorkbook(fi);
 		sheet = workbook.getSheet(sheetName);
 		row = sheet.getRow(rownum);
 		cell = row.getCell(colnum);
-
+ 
 		DataFormatter formatter = new DataFormatter();
 		String data;
 		try {
@@ -69,7 +71,7 @@ public class ExcelUtility {
 		fi.close();
 		return data;
 	}
-
+ 
 	public void setCellData(String sheetName, int rownum, int colnum, String data) throws IOException {
 		File xlfile = new File(path);
 		if (!xlfile.exists()) // If file not exists then create new file
@@ -78,18 +80,18 @@ public class ExcelUtility {
 			fo = new FileOutputStream(path);
 			workbook.write(fo);
 		}
-
+ 
 		fi = new FileInputStream(path);
 		workbook = new XSSFWorkbook(fi);
-
+ 
 		if (workbook.getSheetIndex(sheetName) == -1) // If sheet not exists then create new Sheet
 			workbook.createSheet(sheetName);
 		sheet = workbook.getSheet(sheetName);
-
+ 
 		if (sheet.getRow(rownum) == null) // If row not exists then create new Row
 			sheet.createRow(rownum);
 		row = sheet.getRow(rownum);
-
+ 
 		cell = row.createCell(colnum);
 		cell.setCellValue(data);
 		fo = new FileOutputStream(path);
@@ -98,44 +100,44 @@ public class ExcelUtility {
 		fi.close();
 		fo.close();
 	}
-
-	public void fillGreenColor(String sheetName, int rownum, int colnum) throws IOException {
-		fi = new FileInputStream(path);
-		workbook = new XSSFWorkbook(fi);
-		sheet = workbook.getSheet(sheetName);
-
-		row = sheet.getRow(rownum);
-		cell = row.getCell(colnum);
-
-		style = workbook.createCellStyle();
-
-		style.setFillForegroundColor(IndexedColors.GREEN.getIndex());
-		style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-
-		cell.setCellStyle(style);
-		workbook.write(fo);
-		workbook.close();
-		fi.close();
-		fo.close();
+ 
+	public void createExcelFile(List<WebElement> insurersPlans) throws IOException {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Student Data");
+ 
+        // Create header row
+        XSSFRow headerRow = sheet.createRow(0);
+        String[] headers = {"Insurance Plan", "Policy", "Years", "Amount", "TotalPremiumAmount", "Features", "Included", "Addons", "Addons Available", "Compare"};
+        for (int i=0; i<headers.length; i++) {
+            XSSFCell cell = headerRow.createCell(i);
+            cell.setCellValue(headers[i]);
+        }
+ 
+        // Populate data rows
+        for (int r=0; r<3; r++) {
+            String[] arr = insurersPlans.get(r).getText().split("\n");
+            XSSFRow row = sheet.createRow(r + 1);
+            for (int i=0; i<arr.length; i++) {
+                row.createCell(i).setCellValue(arr[i]);
+            }
+        }
+ 
+        try (FileOutputStream fileOut = new FileOutputStream(path)) {
+            workbook.write(fileOut);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Close the workbook
+        try {
+            workbook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+ 
+		System.out.println("Excel file created successfully!");
+ 
 	}
-
-	public void fillRedColor(String sheetName, int rownum, int colnum) throws IOException {
-		fi = new FileInputStream(path);
-		workbook = new XSSFWorkbook(fi);
-		sheet = workbook.getSheet(sheetName);
-		row = sheet.getRow(rownum);
-		cell = row.getCell(colnum);
-
-		style = workbook.createCellStyle();
-
-		style.setFillForegroundColor(IndexedColors.RED.getIndex());
-		style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-
-		cell.setCellStyle(style);
-		workbook.write(fo);
-		workbook.close();
-		fi.close();
-		fo.close();
-	}
-
+ 
+	
+ 
 }
